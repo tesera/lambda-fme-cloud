57d959df12a2eded9a8a0ff7207d9ec5f4310913
zmpeg <zandstra.matt@gmail.com>
2017-05-09 16:15:09 -0600

    added fme job check complete function

--- handler.js
+++ handler.js
@@ -1,17 +1,13 @@
 'use strict';
 
 const FMECloudAPI = require('fme-cloud-api');
+const FMEServerAPI = require('fme-server-api');
 
 module.exports.start = (event, context, callback) => {
     if(event.FMEInstanceID) {
         var client = new FMECloudAPI(process.env.TOKEN);
         client.start(event.FMEInstanceID);
-        callback(null, {
-            message: 'lambda-fme-cloud (start) called successfully!',
-            FMEInstanceID: event.FMEInstanceID,
-            started: true,
-            event
-        });
+        callback(null, event);
     } else {
         callback({
             message: 'Missing FMEInstanceID',
@@ -24,12 +20,7 @@ module.exports.pause = (event, context, callback) => {
     if(event.FMEInstanceID) {
         var client = new FMECloudAPI(process.env.TOKEN);
         client.pause(event.FMEInstanceID).then(() => {
-            callback(null, {
-                message: 'lambda-fme-cloud (pause) called successfully!',
-                FMEInstanceID: event.FMEInstanceID,
-                paused: true,
-                event
-            });
+            callback(null, event);
         }).catch(console.error);
     } else {
         callback({
@@ -50,3 +41,24 @@ module.exports.list = (event, context, callback) => {
         });
     }).catch(callback);
 };
+
+module.exports.submitJob = (event, context, callback) => {
+    var client = new FMEServerAPI(process.env.SERVER, process.env.SERVER_TOKEN);
+
+    client.submitJob(event.job.repository, event.job.workspace, event.job.parameters).then((res) => {
+        event.jobOutput = res
+        callback(null, event);
+    })
+    .catch(callback);
+}
+
+module.exports.checkJobComplete = (event, context, callback) => {
+    var client = new FMEServerAPI(process.env.SERVER, process.env.SERVER_TOKEN);
+
+    client.getJob(event.jobID).then((res) => {
+        res = JSON.parse(res);
+        console.log(`Job ${event.jobID} is ${res.status}`);
+        callback(null, res.status);
+    })
+    .catch(callback);
+}
